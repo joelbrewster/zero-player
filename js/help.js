0a208d6ae5f996535deba9cfcd2e98e1dbacbe9f
Joel Brewster <j.n.brewster@gmail.com>
2016-07-19 17:01:59 +1000

    Changed help dialogue

--- js/help.js
+++ js/help.js
@@ -1,5 +1,7 @@
 
 function help()
 {
-  vex.dialog.alert('Hi. Try these patterns. <img src="images/patterns.png" width="420" height="320" alt="Patterns" />');
+  vex.dialog.buttons.YES.text = 'Start';
+  vex.dialog.alert('Copy the shapes below onto the grid: <img src="images/patterns.png" width="420" height="320" alt="Patterns" />');
+  // vex.dialog.alert('Hi. <br> Try pressing enough buttons to make a shape.');
 }
