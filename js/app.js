3aa82f958469086a69d42aec7fcaa1e46f1bfc81
Joel Brewster <j.n.brewster@gmail.com>
2016-08-19 13:27:23 +1000

    Cleanup

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-// Use monoGrid later to send osc data
+//TODO Use monoGrid to send OSC data from js to MAXMSP - Set lights on the monome as user feedback
+//TODO Send osc data back to the monome
+// http://monome.org/
+
+
+// Draw the grid array
 var monoGrid = new Array(8);
 
 n = 8;
 m = 8;
 
+// Loop over the array
 for (var i = 0; i < n; i++) {
   monoGrid[i] = new Array(8);
   for (var j = 0; j < m; j++) {
@@ -11,12 +17,14 @@ for (var i = 0; i < n; i++) {
   }
 }
 
+//Set the audio effects
 var ping = new Tone.PingPongDelay(0.16, 0.7).toMaster();
 
 var filter = new Tone.Filter(10, "lowpass").connect(ping);
 
-//a polysynth composed of 6 Voices of MonoSynth
+//3 voices for the synth, sounds more chordal
 var synth = new Tone.PolySynth(3, Tone.MonoSynth).connect(filter);
+
 //set the attributes using the set interface
 synth.set({
   volume: -25,
@@ -46,34 +54,33 @@ synth.set({
 });
 
 nx.onload = function() {
+  // Set help option for user to click the "?" on the screen or on the keyboard
   help();
 
   // For sending back lights to the monome
+  //TODO Set this up with an ajax server when time
+
   // nx.sendsTo("ajax");
   // nx.setAjaxPath("lib/nexusOSCRelay.php");
+
+  // Set colors
   nx.colorize("#2d2d2d");
   nx.colorize("accent", "#0099CC");
   nx.colorize("fill", "#2D2D2D");
-  // Change matrix to be lighter?
-  // nx.colorize.matrix1("fill", "#989898");
   nx.colorize("border", "#ffffff");
   matrix1.cellBuffer = 8;
 
-  matrix1.col = 8;
-  matrix1.row = 8;
-
-  // Try to get the qwerty keys on the grid. Not sure if it'll look good
+  // Start
   matrix1.init();
+
+  // Set the life span for the matrix cells
   setInterval(matrix1.life, 80);
 
-  // this.mode = "matrix";
-	// this.matrixLabels = true;
-  // this.matrixLabels = ["1","2","3","4","5","6","7","8","q","w","e","r","t","y","u","i","a","s","d","f","g","h","j","k","z","x","c","v","b","n","m",",","!","@","#","","$","","%","","^","","&","","*","Q","W","E","R","T","Y","U","I","A","S","D","F","G","H","J","K","Z","X","C","V","B","N","M","<"];
-  // this.init();
-
   matrix1.on('*', function(data) {
+    // Make the audio more interesting
     synth.set("detune", ~~(Math.random() * 20 - 1));
 
+    // Get i in grid
     if (data.grid) {
       for (var i = 0; i < data.grid.length; i++) {
         for (var j = 0; j < data.grid[i].length; j++) {
@@ -83,15 +90,15 @@ nx.onload = function() {
           }
 
           if (data.grid[i][j] != monoGrid[i][j]) {
-            // do monome stuff
+            // TODO: send data back to the monome
             if (data.grid[i][j] == 1) {
-              // turnOn monome
+              // turnOn monome lights
               // console.log(i, j, 1);
               matrix1.transmit(i, j);
               monoGrid[i][j] = 1;
             }
             if (data.grid[i][j] === 0) {
-              // turnOff monome
+              // turnOff monome lights
               monoGrid[i][j] = 0;
               // console.log(i, j, 0);
               monoGrid[i][j] = 0;
@@ -103,12 +110,14 @@ nx.onload = function() {
   });
   matrix1.draw();
 
+  // Envelope
   envelope1.on('*', function(data) {
     if (data.amp) {
       filter.frequency.value = data.amp * 2000;
     }
   });
 
+  // Set the sound envelope: ADSR
   envelope1.val.points = [{
     x: 0.0,
     y: 0.0
@@ -121,10 +130,10 @@ nx.onload = function() {
   }, {
     x: 1.0,
     y: 0.0
-    }];
-    envelope1.draw();
-    envelope1.looping = true;
-    envelope1.duration = 8192;
-    envelope1.start();
+  }];
+  envelope1.draw();
+  envelope1.looping = true;
+  envelope1.duration = 8192;
+  envelope1.start();
 
-  };
+};
